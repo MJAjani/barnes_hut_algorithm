cbc515dc1400dce300076c3b52e834363dd4b6d3
Orchidoclast <36733025+Orchidoclast@users.noreply.github.com>
2018-03-15 14:46:50 +0100

    Update code1.py
    need other comments

--- code1.py
+++ code1.py
@@ -24,43 +24,45 @@ def processMatrix(M, level):
   #------------------------------
 
 def printMatrix(M, level, leaf):
+  #On realise l'arborescence, et on l'affiche
   tab = repeat_to_length('\t',level+1)
   if leaf:
     print(tab + 'leaf')
   else:
     print(tab + 'branch')
 
-def repeat_to_length(string_to_expand, length):
+def repeat_to_length(string_to_expand, length): #EXPLAIN
     return (string_to_expand * (int(length/len(string_to_expand))+1))[:length]
 
 def initMatrix(n,m):
   return np.zeros(shape=(n,m)) # init a matrix 8/8
 
-def initPoints(M):#TODO randomize
-  size = M.shape
-  n = size[0]
-  m = size[1]
-  maxPoints = randint(4, int(n*m/6))
-  for i in range(maxPoints): 
+def initPoints(M):
+  size = M.shape()
+  n = size[0] #nombre lignes matrice
+  m = size[1] #nombre colonnes matrice
+  maxPoints = randint(4, int(n*m/6)) #Obtention aleatoire d'un nombre de points
+  for i in range(maxPoints): #On place les points de coordonnees aleatoires
     (x,y) = getRandomPoints(m,n)
     M[x,y] = 1
   return M
 
 def getRandomPoints(m,n):
+  #Pour obtenir des points aléatoires
   x = randint(0, m-1)
   y = randint(0, n-1)
   return (x,y)
 
 def decoupe(M):
   size = M.shape # (8,8)
-  n = size[0]
-  m = size[1]
-  newSizeN = n // 2
-  newSizeM = m // 2
-  M1 = M[0:newSizeN,0:newSizeM]
-  M2 = M[newSizeN:n,0:newSizeM]
-  M3 = M[0:newSizeN,newSizeM:m]
-  M4 = M[newSizeN:n,newSizeM:m]
+  n = size[0] #taille d'une ligne
+  m = size[1] # taille d'une colonne
+  newSizeN = n // 2 #taille d'une ligne coupee en deux
+  newSizeM = m // 2 #taille d'une colonne coupee en deux
+  M1 = M[0:newSizeN,0:newSizeM] #Matrice nw
+  M2 = M[newSizeN:n,0:newSizeM] #Matrice sw
+  M3 = M[0:newSizeN,newSizeM:m] #Matrice ne
+  M4 = M[newSizeN:n,newSizeM:m] #Matrice se
   return (M1, M2, M3, M4)
 
 def countPoints(M):
@@ -72,7 +74,7 @@ def countPoints(M):
 ############################################
 ############################################
 
-# Init our system
+"""# Init our system
 i = randint(2, 9)
 n = i*2
 m = n # to have a square matrix
@@ -89,4 +91,4 @@ processMatrix(M, 0)
 # faire le CALCULATING THE FORCE ACTING ON A BODY
 # 
 # 
-# 
+# """
